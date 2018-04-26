dff9d026f7f73a877f2268eaa3216c7882f84c8a
babacan123456 <38765561+babacan123456@users.noreply.github.com>
2018-04-26 22:16:40 +0300

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,15 @@
+const  Discord  =  gerektirir ( ' discord.js ' );
+const  client  =  yeni  Discord.Client ();
+
+istemci . on ( ' hazır ' , () => {
+    Konsol . log ( ' Hazırım! ' );
+});
+
+istemci . on ( ' message ' , mesaj  => {on
+    eğer ( mesaj . content  ===  ' ping ' ) {
+    	mesajı . cevap ( ' pong ' );
+  	}
+});
+
+// BU ŞEKİLDE OLMALIDIR
+istemci . giriş ( süreç . env .NDM4MzE3MDQ5ODkxMTI3Mjk3.DcO2dw.pZ2S5HkJsTOjn3L4Wt4ADTZSndY);
